1f301a4f8eabf37e6d34d68eba0f5d34f59ba77a
zohaib choudhary <zohaibchoudhry21@gmail.com>
2026-07-26 04:24:42 +0500

    Remove venv from git tracking

--- python-service/venv/Lib/site-packages/pymupdf/_build.py
+++
@@ -1,9 +0,0 @@
-mupdf_location = 'https://mupdf.com/downloads/archive/mupdf-1.27.2-source.tar.gz'
-pymupdf_version = '1.27.2.3'
-pymupdf_version_tuple = (1, 27, 2, 3)
-pymupdf_git_sha = ''
-pymupdf_git_diff = ''
-pymupdf_git_branch = 'aptest-jules'
-swig_version = '4.4.1'
-swig_version_tuple = (4, 4, 1)
-fake_no_gil = False
